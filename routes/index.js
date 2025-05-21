26c980de428f10e342230fe4d5a92fd1698efa3c
KWAYU MMARI <84182991+kwayummari@users.noreply.github.com>
2025-05-21 12:43:46 +0300

    fixing routes

--- routes/index.js
+++ routes/index.js
@@ -1,20 +1,27 @@
 const express = require('express');
 const authRoutes = require('./authRoutes');
-// Import other route modules here
+const userRoutes = require('./userRoutes');
+const departmentRoutes = require('./departmentRoutes');
+const leaveRoutes = require('./leaveRoutes');
+const attendanceRoutes = require('./attendanceRoutes');
+const documentRoutes = require('./documentRoutes');
+const roleRoutes = require('./roleRoutes');
 
 const router = express.Router();
 
 // Register routes
 router.use('/auth', authRoutes);
-// Register other routes here
-// router.use('/users', userRoutes);
-// router.use('/products', productRoutes);
-// etc.
+router.use('/users', userRoutes);
+router.use('/departments', departmentRoutes);
+router.use('/leaves', leaveRoutes);
+router.use('/attendance', attendanceRoutes);
+router.use('/documents', documentRoutes);
+router.use('/roles', roleRoutes);
 
-// Root API information route
+// Root API information
 router.get('/', (req, res) => {
   res.json({
-    message: 'Welcome to the API',
+    message: 'Welcome to the GHF HR System API',
     version: '1.0.0',
     documentation: '/api-docs',
   });
