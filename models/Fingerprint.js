de9f6cc3a99d1e59500290f19c0b191433d72ed3
KWAYU MMARI <84182991+kwayummari@users.noreply.github.com>
2025-05-21 13:13:13 +0300

    fixing models

--- models/Fingerprint.js
+++ models/Fingerprint.js
@@ -1,42 +1,56 @@
-// const { DataTypes } = require('sequelize');
-// const sequelize = require('../config/dbConfig');
+const { DataTypes } = require('sequelize');
+const sequelize = require('../config/dbConfig');
 
-// /**
-//  * Fingerprint model
-//  * @swagger
-//  * components:
-//  *   schemas:
-//  *     Fingerprint:
-//  *       type: object
-//  *       required:
-//  *         - user_id
-//  *         - template
-//  *       properties:
-//  *         id:
-//  *           type: integer
-//  *           description: Fingerprint ID
-//  *         user_id:
-//  *           type: integer
-//  *           description: User ID
-//  *         template:
-//  *           type: string
-//  *           description: Fingerprint template data
-//  */
-// const Fingerprint = sequelize.define('Fingerprint', {
-//   user_id: {
-//     type: DataTypes.INTEGER,
-//     allowNull: false,
-//     references: {
-//       model: 'users',
-//       key: 'id',
-//     },
-//   },
-//   template: {
-//     type: DataTypes.TEXT('long'),
-//     allowNull: false,
-//   },
-// }, {
-//   tableName: 'fingerprints',
-//   timestamps: true,
-//   underscored: true,
-//   createdAt: 'create
+/**
+ * Fingerprint model
+ * @swagger
+ * components:
+ *   schemas:
+ *     Fingerprint:
+ *       type: object
+ *       required:
+ *         - user_id
+ *         - template
+ *       properties:
+ *         id:
+ *           type: integer
+ *           description: Fingerprint ID
+ *         user_id:
+ *           type: integer
+ *           description: User ID
+ *         template:
+ *           type: string
+ *           description: Fingerprint template data
+ */
+const Fingerprint = sequelize.define('Fingerprint', {
+  user_id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    references: {
+      model: 'users',
+      key: 'id',
+    },
+    onUpdate: 'CASCADE',
+    onDelete: 'CASCADE',
+  },
+  template: {
+    type: DataTypes.TEXT('long'),
+    allowNull: false,
+  },
+}, {
+  tableName: 'fingerprints',
+  timestamps: true,
+  underscored: true,
+  createdAt: 'created_at',
+  updatedAt: false, // No updated_at field in this table
+});
+
+// Model associations
+Fingerprint.associate = (models) => {
+  Fingerprint.belongsTo(models.User, {
+    foreignKey: 'user_id',
+    as: 'user',
+  });
+};
+
+module.exports = Fingerprint;
