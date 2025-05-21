f2c96457a5637dee4869d0cde0f3efb872eef3e3
KWAYU MMARI <84182991+kwayummari@users.noreply.github.com>
2025-05-21 13:37:56 +0300

    fixing models

--- models/index.js
+++ models/index.js
@@ -1,16 +1,93 @@
 const Sequelize = require('sequelize');
-const config = require('../config/config');
 const sequelize = require('../config/dbConfig');
 const logger = require('../utils/logger');
 
-// Import models
+// Import all models
 const User = require('./User');
-// Import other models here
+const Department = require('./Department');
+const BasicEmployeeData = require('./BasicEmployeeData');
+const BioData = require('./BioData');
+const PersonalEmployeeData = require('./PersonalEmployeeData');
+const EmergencyContact = require('./EmergencyContact');
+const NextOfKin = require('./NextOfKin');
+const Fingerprint = require('./Fingerprint');
+const LeaveType = require('./LeaveType');
+const LeaveApplication = require('./LeaveApplication');
+const WorkScheduler = require('./WorkScheduler');
+const HolidayList = require('./HolidayList');
+const Attendance = require('./Attendance');
+const Document = require('./Document');
+const Role = require('./Role');
+const Permission = require('./Permission');
+const UserRole = require('./UserRole');
+const RolePermission = require('./RolePermission');
+const FiscalYear = require('./FiscalYear');
+const Quarter = require('./Quarter');
+const Budget = require('./Budget');
+const BudgetExpense = require('./BudgetExpense');
+const PurchaseRequest = require('./PurchaseRequest');
+const Supplier = require('./Supplier');
+const Quotation = require('./Quotation');
+const LPO = require('./LPO');
+const FlatRate = require('./FlatRate');
+const TravelAdvanceRequest = require('./TravelAdvanceRequest');
+const ExpenseReport = require('./ExpenseReport');
+const ExpenseLine = require('./ExpenseLine');
+const PettyCashBook = require('./PettyCashBook');
+const PettyCashExpense = require('./PettyCashExpense');
+const ReplenishmentRequest = require('./ReplenishmentRequest');
+const MeetingRecord = require('./MeetingRecord');
+const MeetingTask = require('./MeetingTask');
+const Notification = require('./Notification');
+const Objective = require('./Objective');
+const AppraisalForm = require('./AppraisalForm');
+const Increment = require('./Increment');
+const Payroll = require('./Payroll');
+const SalaryComponent = require('./SalaryComponent');
 
-// Initialize models
+// Initialize models object with all models
 const models = {
   User,
-  // Add other models here
+  Department,
+  BasicEmployeeData,
+  BioData,
+  PersonalEmployeeData,
+  EmergencyContact,
+  NextOfKin,
+  Fingerprint,
+  LeaveType,
+  LeaveApplication,
+  WorkScheduler,
+  HolidayList,
+  Attendance,
+  Document,
+  Role,
+  Permission,
+  UserRole,
+  RolePermission,
+  FiscalYear,
+  Quarter,
+  Budget,
+  BudgetExpense,
+  PurchaseRequest,
+  Supplier,
+  Quotation,
+  LPO,
+  FlatRate,
+  TravelAdvanceRequest,
+  ExpenseReport,
+  ExpenseLine,
+  PettyCashBook,
+  PettyCashExpense,
+  ReplenishmentRequest,
+  MeetingRecord,
+  MeetingTask,
+  Notification,
+  Objective,
+  AppraisalForm,
+  Increment,
+  Payroll,
+  SalaryComponent
 };
 
 // Associate models
